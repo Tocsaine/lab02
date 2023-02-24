f879b35ad96fb341935729d872775c70d07dd934
Tocsaine <navi1301@gmail.com>
2023-02-24 18:07:05 +0300

    breaking master

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -7,5 +7,5 @@
 int main(){
     std::string name;
     std::cin >> name;
-    std::cout << "Hello world from " + name << std::endl; //Some comment
+    std::cout << "Hello world from " + name << std::endl; //Some comment, какой-то комментарий!
 }
