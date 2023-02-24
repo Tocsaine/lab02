8ab3df70ffc2ad5f156c554f3e9c36f4228aaf6e
Tocsaine <navi1301@gmail.com>
2023-02-24 17:41:31 +0300

    Hello world with comments

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -7,5 +7,5 @@
 int main(){
     std::string name;
     std::cin >> name;
-    std::cout << "Hello world from " + name << std::endl;
+    std::cout << "Hello world from " + name << std::endl; //Some comment
 }
